bd1f96db2c00a7b9c20834775fbd75c6657956dc
Ashwin Nishad <ashwinnishad@Ashwins-MacBook-Air.local>
2024-04-02 16:20:17 -0700

    loaded penguin dataset to visualize their bill measurements using matplotlib

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
+# app.py
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 import streamlit as st
 
 st.set_page_config(
-    page_title="Data Visualization App", 
-    page_icon="📊", 
+    page_title="Penguin Data Visualization",
+    page_icon="🐧",
     layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
 )
 
-st.title("Data Visualization App")
+st.title("🐧 Penguins Data Visualization")
+
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/mcnakhaee/palmerpenguins/master/palmerpenguins/data/penguins.csv"
+)
+
+st.write(df)
+
+# scatter plot
+plot, axs = plt.subplots()
+sns.scatterplot(data=df, x="bill_length_mm", y="bill_depth_mm", hue="species", ax=axs)
+axs.set_title("Penguin Bill Measurements")
+axs.set_xlabel("Bill Length (mm)")
+axs.set_ylabel("Bill Depth (mm)")
+
+st.pyplot(plot)
