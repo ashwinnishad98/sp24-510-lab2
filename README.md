c72848e04e29b9e92a64a64de2d81e71ecc2e2a8
Ashwin Nishad <ashwinnishad@Ashwins-MacBook-Air.local>
2024-04-08 16:04:40 -0700

    updated app.py to reflect data viz of titanic data, and updated readme

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
 # Lab 2
-An interactive data visualization application.
+An interactive application to view the dataset of the passengers aboard the Titanic. The dataset contains information pertaining to the passengers, including whether they survived or not. Filters are present on the sidebar, that users can apply which dynamically changes the graphs plotted. Two graphs are plotted in this application:
 
-## Getting Started
+    1. Bar graph that shows the survival rates amongst passengers in each class i.e. 1st class, 2nd class, 3rd class.
+    2. Scatter plot that shows the distribution of Age and Gender by Survival Status on the Titanic.
+
+
+## How to Run
+Open your terminal (if in VS Code, press ```ctrl + ` ```).
+
+```
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+streamlit run app.py
+```
+
+## What's Included
+- app.py: The entry point to the data visualization application. It contains streamlit code to display the graphs and plots, and dataframe operations using the Pandas library to filter specified data.
+
 
 ## Lessons Learned
+- Using `plotly_express` package to plot graphs, and the various customization options it offers.
+- Learned about Streamlit's dynamic behavior, when filters are applied to a dataframe.
 
 ## Questions
+- Can interactions with the plots be reflected in the filter values?
--- app.py
+++ app.py
@@ -1,30 +1,127 @@
 # app.py
-import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
+import plotly.express as px
 import streamlit as st
 
 st.set_page_config(
-    page_title="Penguin Data Visualization",
-    page_icon="🐧",
+    page_title="Titanic Data Visualization",
+    page_icon="🛳️",
     layout="centered",
     initial_sidebar_state="auto",
     menu_items=None,
 )
 
-st.title("🐧 Penguins Data Visualization")
+st.title("🛳️ Titanic Data Visualization")
 
 df = pd.read_csv(
-    "https://raw.githubusercontent.com/mcnakhaee/palmerpenguins/master/palmerpenguins/data/penguins.csv"
+    "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
 )
 
-st.write(df)
+survival_options = {1: "Yes", 0: "No"}
 
-# scatter plot
-plot, axs = plt.subplots()
-sns.scatterplot(data=df, x="bill_length_mm", y="bill_depth_mm", hue="species", ax=axs)
-axs.set_title("Penguin Bill Measurements")
-axs.set_xlabel("Bill Length (mm)")
-axs.set_ylabel("Bill Depth (mm)")
+with st.sidebar:
+    # Filter options
 
-st.pyplot(plot)
+    # slider for fare range
+    fare_range_slider = st.slider(
+        "Fare Range ($)", min(df["Fare"]), max(df["Fare"]), value=max(df["Fare"])
+    )
+
+    # selectbox for survivor filter
+    survivor_filter = st.selectbox(
+        "Survived?", options=list(survival_options.values()), index=None
+    )
+
+    # multiselect for passenger class filter
+    pclass_filter = st.multiselect(
+        "Passenger Class", options=df["Pclass"].unique().tolist()
+    )
+
+filtered_df = df.copy()
+
+# Update the dataframe based on filters applied
+if pclass_filter:
+    filtered_df = filtered_df[filtered_df["Pclass"].isin(pclass_filter)]
+
+if survivor_filter:
+    survivor_filter_value = {v: k for k, v in survival_options.items()}[survivor_filter]
+    filtered_df = filtered_df[filtered_df["Survived"] == survivor_filter_value]
+
+filtered_df = filtered_df[filtered_df["Fare"] < fare_range_slider]
+
+# Calculate survival rates by passenger class
+survival_rates = filtered_df.groupby("Pclass")["Survived"].mean().reset_index()
+survival_rates["Pclass"] = survival_rates["Pclass"].astype(str)
+
+# Show RAW and Filtered data
+with st.expander("RAW Data"):
+    st.write(df)
+
+with st.expander("Filtered Data"):
+    st.write(filtered_df)
+
+# Display dataset description
+st.markdown(
+    """
+This dataset contains information about the passengers on the Titanic, along with information relating to their survival.
+Descriptions of each column:
+- **Survived**: Whether the passenger survived or not (0 = No, 1 = Yes)
+- **Pclass**: Passenger class (1 -> 1st class, 2 -> 2nd class, 3 -> 3rd class)
+- **Sex**: Gender
+- **Age**: Age
+- **Fare**: Fare paid
+- **Embarked**: Port of embarkation (C -> Cherbourg, Q -> Queenstown, S -> Southampton)
+- **SibSp**: Number of siblings/spouses aboard
+- **Parch**: Number of parents/children aboard
+- **Cabin**: Cabin number
+- **Ticket**: Ticket number
+"""
+)
+
+# Plot a bar chart to show survival rates amongst each passenger class
+fig = px.bar(
+    survival_rates,
+    x="Pclass",
+    y="Survived",
+    labels={"Survived": "Survival Rate", "Pclass": "Passenger Class"},
+    text="Survived",
+    title="Survival Rates Across Different Passenger Classes on the Titanic",
+)
+
+# Update the text on each bar to display percentages
+fig.update_traces(texttemplate="%{text:.2f}", textposition="outside")
+
+# Updating layout of the plot to add labels
+fig.update_layout(
+    xaxis_title="Passenger Class",
+    yaxis_title="Survival Rate",
+    xaxis=dict(
+        tickmode="array",
+        tickvals=[1, 2, 3],
+        ticktext=["1st Class", "2nd Class", "3rd Class"],
+    ),
+)
+
+st.plotly_chart(fig)
+
+df_filtered_age = filtered_df.dropna(subset=["Age"])
+# Scatter plot to show age and gender distribution by survival status
+fig2 = px.scatter(
+    df_filtered_age,
+    x="Sex",
+    y="Age",
+    color="Survived",
+    color_continuous_scale=px.colors.sequential.Cividis,
+    labels={"Survived": "Survival Status"},
+    title="Age and Gender Distribution by Survival Status on the Titanic",
+)
+
+# Update the color axis for clarity
+fig2.update_coloraxes(colorbar_title="Survived<br>(1 = Survived, 0 = Not Survived)")
+
+fig2.update_layout(
+    width=950,
+    height=800,
+)
+
+st.plotly_chart(fig2)
